7034896177066f2d5d4074ba39e7620262b7c33e
Edder H F <edhernandezfo@unal.edu.co>
2026-01-09 20:45:32 -0500

    :refactor: refactor: simplify platform controller

--- controllers/PlatformController.php
+++ controllers/PlatformController.php
@@ -1,16 +1,13 @@
 <?php
 require_once(__DIR__ . '/../models/PlatformModel.php');
-function listPlatforms()
-{
-    $platformModel = new Platform(null, null);
-    $platforms = $platformModel->getAll();
-    $platformArray = [];
 
-    foreach ($platforms as $platform) {
-        $platformObj = new Platform($platform->getId(), $platform->getName());
-        $platformArray[] = $platformObj;
+class PlatformController
+{
+    public function index()
+    {
+        $platforms = Platform::getAll();
+        require_once (__DIR__ .'/../views/platforms/list-platforms.php');
     }
-    return $platformArray;
 }
 
 ?>

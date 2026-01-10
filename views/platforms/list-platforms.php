ae73c3e7b4274abf39a3df9f0153015d02db20f4
Edder H F <edhernandezfo@unal.edu.co>
2026-01-09 21:05:03 -0500

    ui: adapted list view for platform refactor

--- views/platforms/list-platforms.php
+++ views/platforms/list-platforms.php
@@ -1,22 +1,27 @@
 <?php
-require_once(dirname(__DIR__, 2) . '/controllers/PlatformController.php');
-$platforms = listPlatforms();
+require_once(__DIR__ . '/../../controllers/PlatformController.php');
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biblioteca de series | Plataformas</title>
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="./views/styles/styles.css">
-    <link rel="shortcut icon" href="./views/imgs/video-folder.png" type="image/x-icon">
+    <link rel="stylesheet" href="../styles/styles.css">
+    <link rel="shortcut icon" href="../imgs/video-folder.png" type="image/x-icon">
 </head>
+
 <body>
-    <section class="container centered-content"> 
+    <section class="container centered-content">
         <nav class="navbar navbar-expand-lg navbar-light align-items-center mb-4">
             <a class="navbar-brand" href="../../index.html">
                 <img src="../imgs/video-folder.png" alt="Logo" width="30" height="30" class="d-inline-block align-top">
@@ -35,28 +40,35 @@ $platforms = listPlatforms();
     </section>
     <section class="container centered-content">
         <div class="container">
-            <button class="btn btn-success mb-3 ">+ Añadir Plataforma</button>
-            <?php if (count($platforms) > 0){ 
-            ?>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Id</th>
-                        <th scope="col" class="w-50">Nombre</th>
-                        <th scope="col">Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($platforms as $platform) { ?>
-                    <tr>
-                        <th scope="row"><?php echo $platform->getId(); ?></th>
-                        <td><?php echo $platform->getName(); ?></td>
-                        <td>
-                            <a href="#" class="btn btn-primary btn-sm">Editar</a>
-                            <a href="#" class="btn btn-danger btn-sm">Eliminar</a>
-                        </td>
-                    </tr>
-                    <?php } ?>
+            <a href="?entity=platforms&action=create" class="btn btn-success mb-3 "><i
+                    class="bi bi-plus-circle"></i> Añadir Plataforma</a>
+            <?php if (!empty($platforms)) {
+                ?>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col" class="w-50">Nombre</th>
+                            <th scope="col">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($platforms as $platform) { ?>
+                            <tr>
+                                <th scope="row"><?php echo htmlspecialchars($platform->getId()); ?></th>
+                                <td><?php echo htmlspecialchars($platform->getName()); ?></td>
+                                <td>
+                                    <a href="index.php?entity=platforms&action=edit&id=<?php echo $platform->getId(); ?>"
+                                        class="btn btn-primary btn-sm">
+                                        <i class="bi bi-pencil"></i> Editar
+                                    </a>
+                                    <a href="index.php?entity=platforms&action=delete&id=<?php echo $platform->getId(); ?>"
+                                        class="btn btn-danger btn-sm">
+                                        <i class="bi bi-trash"></i> Eliminar
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php } ?>
                     <?php } else { ?>
                         <div class="alert alert-warning" role="alert">Aun no existen plataformas</div>
                     <?php } ?>
@@ -65,4 +77,5 @@ $platforms = listPlatforms();
         </div>
     </section>
 </body>
+
 </html>
---
+++ views/platforms/temporalRouter.php
@@ -0,0 +1,5 @@
+<?php
+require_once(__DIR__ . '/../../controllers/PlatformController.php');
+$platformController = new PlatformController();
+$platformController->index();
+?>
